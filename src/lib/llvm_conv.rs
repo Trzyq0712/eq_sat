76f3c66cc4bd7010b3ff56bfdecb80cec49181b8
Trzyq0712 <j.trzyq@gmail.com>
2023-05-08 22:38:07 +0200

    Improve llvm conversion

--- src/lib/llvm_conv.rs
+++ src/lib/llvm_conv.rs
@@ -8,8 +8,7 @@ type LangEgraph = EGraph<Lang, analysis::ConstFold>;
 pub struct FunctionEgraph {
     egraph: LangEgraph,
     root: Id,
-    parameters: Vec<llvm_ir::function::Parameter>,
-    return_type: llvm_ir::TypeRef,
+    function: Function,
 }
 
 impl From<&llvm_ir::Constant> for crate::Lang {
@@ -41,35 +40,35 @@ impl From<&llvm_ir::Name> for crate::Lang {
     }
 }
 
-// pub fn convert_to_egraph(func: Function) -> FunctionEgraph {
-//     let mut graph = LangEgraph::default();
-//     for block in func.basic_blocks {
-//         match block.term {
-//             llvm_ir::Terminator::Ret(llvm_ir::terminator::Ret{ return_operand, debugloc: _ }) => match return_operand {
-//                 Some(op) => match op {
-//                     llvm_ir::operand::Operand::ConstantOperand(const_ref) => { Lang::from(const_ref.as_ref()); },
-//                     _ => unimplemented!(),
-//                 }
-//                 None => {}
-//             }
-//             _ => {}
-//         }
-//     }
-//     unimplemented!();
-// }
+pub fn convert_to_egraph(function: Function) -> Result<FunctionEgraph, String> {
+    let mut egraph = LangEgraph::default();
+    let mut root = None;
+    for block in function.basic_blocks.iter() {
+        parse_basic_block(block, &mut egraph)?;
+        if let llvm_ir::terminator::Terminator::Ret(ret) = &block.term {
+            let ret_id = egraph.lookup::<Lang>(ret.return_operand.as_ref().unwrap().into()).unwrap();
+            root = Some(ret_id);
+        }
+    }
+    let root = root.unwrap();
+    Ok(FunctionEgraph { egraph, root, function })
+}
 
-fn parse_basic_block(block: &llvm_ir::BasicBlock, egraph: &mut LangEgraph) -> Result<(), &'static str> {
+macro_rules! bininstr_to_node {
+    ($instr:expr, $graph:expr) => {
+        [
+            $graph.add($instr.get_operand0().into()),
+            $graph.add($instr.get_operand1().into())
+        ]
+    };
+}
+
+fn parse_basic_block(block: &llvm_ir::BasicBlock, egraph: &mut LangEgraph) -> Result<(), String> {
     use llvm_ir::Instruction as Instr;
     for instr in block.instrs.iter() {
         let node = match instr {
-            Instr::Add(instr) => Lang::Add([
-                        egraph.add(instr.get_operand0().into()),
-                        egraph.add(instr.get_operand1().into())
-            ]),
-            Instr::Sub(instr) => Lang::Sub([
-                        egraph.add(instr.get_operand0().into()),
-                        egraph.add(instr.get_operand1().into())
-            ]),
+            Instr::Add(instr) => Lang::Add(bininstr_to_node!(instr, egraph)),
+            Instr::Sub(instr) => Lang::Sub(bininstr_to_node!(instr, egraph)),
             _ => todo!(),
         };
         let name = match instr {
@@ -77,9 +76,9 @@ fn parse_basic_block(block: &llvm_ir::BasicBlock, egraph: &mut LangEgraph) -> Re
             Instr::Sub(instr) => (&instr.dest).into(),
             _ => todo!(),
         };
-        egraph.add(node);
-        egraph.add(name);
+        let node_id = egraph.add(node);
+        let name_id = egraph.add(name);
+        egraph.union(node_id, name_id);
     }
-
     Ok(())
 }
